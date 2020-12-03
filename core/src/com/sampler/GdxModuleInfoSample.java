7cff52a5c00eb86e48d00d5bd4aff5c0ee4dfa73
Marc D. Holman <marcholman9323@gmail.com>
2020-12-03 14:13:09 -0600

    GdxModuleInfoSample

--- core/src/com/sampler/GdxModuleInfoSample.java
+++ core/src/com/sampler/GdxModuleInfoSample.java
@@ -1,33 +1,54 @@
 package com.sampler;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Logger;
 
-public class GdxModuleInfoSample extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class GdxModuleInfoSample implements ApplicationListener {
+
+	private static final Logger log = new Logger(GdxModuleInfoSample.class.getName(), Logger.DEBUG);
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
+
+		log.debug("app= " + Gdx.app);
+		log.debug("audio= " + Gdx.audio);
+		log.debug("input= " + Gdx.input);
+		log.debug("files= " + Gdx.files);
+		log.debug("net= " + Gdx.net);
+		log.debug("graphics= " + Gdx.graphics);
+
+
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void resize(int width, int height) {
+
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void render() {
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void dispose() {
+
 	}
 }
--- desktop/src/com/sampler/desktop/DesktopLauncherGdxModuleInfoSample.java
+++ desktop/src/com/sampler/desktop/DesktopLauncherGdxModuleInfoSample.java
@@ -1,12 +1,22 @@
 package com.sampler.desktop;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sampler.GdxModuleInfoSample;
 import com.sampler.GdxSamplerGame;
 
 public class DesktopLauncherGdxModuleInfoSample {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new GdxSamplerGame(), config);
+
+		System.out.println("app= " + Gdx.app);
+		System.out.println("input= "+ Gdx.input);
+
+		new LwjglApplication(new GdxModuleInfoSample(), config);
+
+		System.out.println("after app= " + Gdx.app);
+		System.out.println("after input= "+ Gdx.input);
+
 	}
 }
