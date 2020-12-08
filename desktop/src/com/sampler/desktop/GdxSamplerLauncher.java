945195aa9a1fb512c15759d6dac97f5c92829432
Marc D. Holman <marcholman9323@gmail.com>
2020-12-08 12:42:57 -0600

    startagain 12-8

--- desktop/src/com/sampler/desktop/GdxSamplerLauncher.java
+++ desktop/src/com/sampler/desktop/GdxSamplerLauncher.java
@@ -1,4 +1,86 @@
 package com.sampler.desktop;
 
-public class GdxSamplerLauncher {
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.lwjgl.LwjglAWTCanvas;
+import com.badlogic.gdx.utils.reflect.ClassReflection;
+import com.badlogic.gdx.utils.reflect.ReflectionException;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+public class GdxSamplerLauncher extends JFrame {
+
+    private static final int WIDTH = 1280;
+    private static final int HEIGHT = 720;
+
+    // abstract window toolkit
+    private LwjglAWTCanvas lwjglAWTCanvas;
+
+    public GdxSamplerLauncher() throws HeadlessException {
+        setTitle(GdxSamplerLauncher.class.getSimpleName());
+        setMinimumSize(new Dimension(WIDTH, HEIGHT));
+        setSize(WIDTH, HEIGHT);
+        setResizable(false);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        // tell window JFrame to resize and layout our components
+        pack();
+        setVisible(true);
+
+        launchSample("com.sampler.InputPollingSample");
+    }
+
+    private void launchSample(String name) {
+        System.out.println("launching sample name = + name");
+
+        Container container = getContentPane();
+
+        if(lwjglAWTCanvas != null) {
+            lwjglAWTCanvas.stop();
+            container.remove(lwjglAWTCanvas.getCanvas());
+        }
+
+        // use reflection to instantiate sample
+
+        ApplicationListener sample;
+
+        try {
+            // get sample class object by name
+            Class<ApplicationListener> clazz = ClassReflection.forName(name);
+
+            // create new instance of our sample class
+            sample = ClassReflection.newInstance(clazz);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Cannot create sample with name= " + name, e);
+        }
+
+        // constructor for LWJGLAWWTCanvas accepts our sample
+        lwjglAWTCanvas = new LwjglAWTCanvas(sample);
+        lwjglAWTCanvas.getCanvas().setSize(WIDTH, HEIGHT);
+        // add lwjglAWTCanvas to Swing JFrame
+        container.add(lwjglAWTCanvas.getCanvas(), BorderLayout.CENTER);
+
+        // pack the layout again as new stuff was added
+        pack();
+
+    }
+
+
+    // main method
+    public static void main(String[] args) {
+        // must be used to run JFrame properly
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new GdxSamplerLauncher();
+            }
+        });
+    }
 }
